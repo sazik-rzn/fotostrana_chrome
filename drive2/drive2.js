4a677d43547ce28dd38631b191e2a762318cb339
sazik-rzn <sazik.rzn@gmail.com>
2018-11-23 17:41:19 +0300

    upd

--- drive2/drive2.js
+++ drive2/drive2.js
@@ -11,11 +11,11 @@ var drive2 = {
             $('.l-body').append('<header class="l-header" data-role="sticky" style="bottom: 10px; background-color:black;"><r-yvnv class="c-top"><r-yvnv class="c-top__container"><r-yvnv class="c-top__wrapper"><r-yvnv id="customMenu" class="c-top__main-nav" data-slot="sitesearch.nav"><r-yvnv class="c-top-nav"><a class="c-top-nav__link" href="http://autogramm.ru" target="_blank">Autogramm(@agmm_channel)<span class="c-notification-bubble c-notification-bubble--top" >!</span></a></r-yvnv></r-yvnv></r-yvnv></r-yvnv></r-yvnv></header>');
         },
         createHref:function(name, href){
-            var link = '<r-yvnv class="c-top-nav"><a href="'+href+'" target="_blank" class="c-top-nav__link" style="cursor:pointer;">' + name + '</a></r-yvnv>';
+            var link = '<r-yvnv class="c-top-nav"><a href="'+href+'" target="_blank" class="c-top-nav__link" style="cursor:pointer;">' + name + '<span class="c-notification-bubble c-notification-bubble--top" >#</span></a></r-yvnv>';
             $(drive2.toolbar.selector).append(link);
         },
         createLink: function (id, name, onclick) {
-            var link = '<r-yvnv class="c-top-nav" id="' + id + '"><a class="c-top-nav__link" style="cursor:pointer;">' + name + '</a></r-yvnv>';
+            var link = '<r-yvnv class="c-top-nav" id="' + id + '"><a class="c-top-nav__link" style="cursor:pointer;">' + name + '<span class="c-notification-bubble c-notification-bubble--top" >0</span></a></r-yvnv>';
             $(drive2.toolbar.selector).append(link);
             $('#' + id).click(function () {
                 onclick();
@@ -29,8 +29,8 @@ var drive2 = {
     },
     automation: {
         selectors: {
-            "Автолайки": '.c-like__button',
-            "Автоподписки": '.c-button--subs',
+            "Автолайки": '.c-like__button:not(.is-pressed)',
+            "Автоподписки": '.c-button--subs:not(.is-pressed)',
         },
         limits:{
 			"Автоподписки": 50
@@ -38,29 +38,36 @@ var drive2 = {
         buttons: {},
         intervals: {},
         counters: {},
+        countersShow: {},
         startButtons: {},
         stopButtons: {},
         start: function (selectorName) {
             $("#" + drive2.automation.startButtons[selectorName]).hide();
             $("#" + drive2.automation.stopButtons[selectorName]).show();
             drive2.automation.intervals[selectorName] = setInterval(() => {
-				if(drive2.automation.limits[selectorName]!==undefined && drive2.automation.counters[selectorName]>=drive2.automation.limits[selectorName]){
-					drive2.automation.stop(selectorName);
-				}
-                if (drive2.automation.counters[selectorName] >= $(drive2.automation.buttons[selectorName]).length - 1) {
+                if (drive2.automation.buttons[selectorName][drive2.automation.counters[selectorName]]===undefined) {    
                     drive2.automation.buttons[selectorName] = $(drive2.automation.selectors[selectorName]);
+                    if(drive2.automation.limits[selectorName]!==undefined && drive2.automation.buttons[selectorName].length>drive2.automation.limits[selectorName]){
+                        drive2.automation.buttons[selectorName].length = drive2.automation.limits[selectorName];
+                    }
+                    drive2.automation.counters[selectorName] = 0;
                 }
                 $(drive2.automation.buttons[selectorName][drive2.automation.counters[selectorName]]).click();
+                 $("#" + drive2.automation.stopButtons[selectorName]+" > a > span").html(drive2.automation.countersShow[selectorName]);
+                
                 $('html, body').animate({
                     scrollTop: $(drive2.automation.buttons[selectorName][drive2.automation.counters[selectorName]]).offset().top - 100
                 }, 0);
                 drive2.automation.counters[selectorName]++;
+                drive2.automation.countersShow[selectorName]++;
             }, 500);
         },
         stop: function (selectorName) {
             $("#" + drive2.automation.startButtons[selectorName]).show();
-            $("#" + drive2.automation.stopButtons[selectorName]).hide();
+            $("#" + drive2.automation.stopButtons[selectorName]).hide();            
             clearInterval(drive2.automation.intervals[selectorName]);
+            drive2.automation.counters[selectorName] = 0;  
+            drive2.automation.buttons[selectorName] = [];
         },
         init: function () {
             $.each(drive2.automation.selectors, function (index, selector) {
@@ -74,6 +81,7 @@ var drive2 = {
                 });
                 $("#" + drive2.automation.stopButtons[index]).hide();
                 drive2.automation.counters[index] = 0;
+                drive2.automation.countersShow[index] = 0;
                 drive2.automation.buttons[index] = [];
             });
         },
